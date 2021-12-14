25a99b67d109086a71965387cac2181fba7c26a6
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2021-12-14 19:55:21 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('.single-item').slick({
+    mobileFirst: true,
+    fade: true,
+    prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
+    nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
+});
