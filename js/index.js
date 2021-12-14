899b47d246ac54b7a689886b528dd0291f8f9686
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2021-12-14 21:41:37 +0530

    made more responsive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
 $('.single-item').slick({
+
     mobileFirst: true,
-    fade: true,
-    prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
-    nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
+
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    infinite: true,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+
+
+
+    // prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
+    // nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
 });
